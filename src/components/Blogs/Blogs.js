b625bc8db52627abdac6342d49ad7354f8b3df41
Bahauddin-Fahad <bahauddin.fahad819@gmail.com>
2022-04-18 23:54:44 +0600

    blog section added

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -13,12 +13,16 @@ const Blogs = () => {
             Answer:
           </span>
           <h6 className="text-justify px-5 pb-3">
-            <strong>The Context API</strong> is a way to declare global
-            variables that can be passed to the other components. Context
-            provides a way to pass data through the component tree without
-            passing props down manually at every level. This is the alternative
-            to <strong>"prop drilling"</strong> which is used to move props from
-            grandparent to parent to child component and so on.
+            <strong className="text-lg">Authentication :</strong> Authentication
+            is to verify the data of a user to give him the access for the next
+            process of website benefits. <br />
+            <strong className="text-lg">Authorization :</strong> Authorization
+            is to give a user permission to use resources or to do further
+            activities.
+            <br />
+            <strong>Simply,</strong> Authentication is the process of verifying
+            who a user is, while Authorization is the process of verifying what
+            they have access to.
           </h6>
         </div>
         <div className="border-2 rounded-lg shadow-[rgba(0,0,0,0.19)_0px_10px_20px,rgba(0,0,0,0.23)_0px_2px_6px]">
@@ -30,12 +34,16 @@ const Blogs = () => {
             Answer:
           </span>
           <h6 className="text-justify px-5 pb-3">
-            <strong>Semantic Tag</strong> is HTML tag that introduces meaning to
-            the web page rather than just presentation.
-            <strong> Semantic HTML tags</strong> gives information about the
-            contents of the tags that describe more about how the contents look
-            on a page. Semantic tags are used when someone want to mark up a
-            content block that has an important role in the document structure.
+            Firebase is less technical and less time consuming than writing
+            complete backend code for dynamic apps. If developer ultimately want
+            to host and manage an app in the cloud and can also consider this
+            tool. Firebase is serverless, so there is no worry about cloud
+            server configuration technology. As a Google Cloud service,
+            developer can also access other Google products and features such as
+            Google Drive and spreadsheets.
+            <br />
+            <strong className="text-left">Some firebase Alternatives : </strong>
+            Parse, Back4App, AWS Amplify, Kuzzle, Couchbase, NativeScript.
           </h6>
         </div>
         <div className="border-2 rounded-lg shadow-[rgba(0,0,0,0.19)_0px_10px_20px,rgba(0,0,0,0.23)_0px_2px_6px]">
@@ -47,15 +55,20 @@ const Blogs = () => {
             Answer:
           </span>
           <h6 className="text-justify px-5 pb-3 max-h-46 overflow-auto">
-            <strong>Inline :</strong> Inline Elements are the elements which
-            don't have fixed height and width. And the height and width values
-            of Inline Elements can't be set manually. <br />
-            <strong>Block : </strong> Block Elements are those which start on a
-            new line and take the available full width. The width and height
-            values of Block elements can be set manually. <br />
-            <strong>Inline-block :</strong> Inline-block Elements are like
-            Inline Elements,but the difference is, the height and width values
-            can be set manually.
+            <strong>Firebase Analytics :</strong> Firebase Analytics presents
+            data about the user behavior of applications and suggests the
+            improvement of the performance.
+            <br />
+            <strong>Cloud Messaging :</strong> Cloud Messaging is a messaging
+            tool that helps the companies to receive and deliver messages on
+            applications at no cost.
+            <br />
+            <strong>Realtime Database :</strong> Realtime Database is a cloud
+            hosted NoSQL Database that stores and syncs data of the users in
+            real time. <br />
+            <strong>Test Lab :</strong> Test Lab is an app testing
+            infrastructure by which developers can test the applications easily.
+            <br />
           </h6>
         </div>
       </div>
